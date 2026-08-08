aa3cee443a7e73ef58b5f6b1e5bf59c6aadb32c7
alefierowicz <mrclalefierowicz@gmail.com>
2026-08-08 04:26:50 +0200

    implemented thread pool as per 21.2

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,22 @@ use std::{
     fs,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+
+use http_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -16,12 +25,13 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader: BufReader<&TcpStream> = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    // println!("got request line: {}", &request_line);
-
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "responses/200.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "responses/400.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "./responses/200.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(2));
+            ("HTTP/1.1 200 OK", "responses/200.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "./responses/404.html"),
     };
 
     let contents = fs::read_to_string(filename).unwrap();
